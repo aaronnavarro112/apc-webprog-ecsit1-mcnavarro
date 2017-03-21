7d9384fb4ddaba748137e1e6e047f4284bf38e3a
Aaron Navarro <aaronnavarro.112@gmail.com>
2017-03-21 10:31:42 +0800

    Added rows in mysql and added the interface to the codeigniter website

--- WEBPROG/CodeIgniter/application/views/news/create.php
+++ WEBPROG/CodeIgniter/application/views/news/create.php
@@ -5,16 +5,36 @@
 <?php echo form_open('news/create'); ?>    
     <table>
         <tr>
-            <td><label for="title">Title</label></td>
+            <td><label for="title">Complete Name</label></td>
+            <td><input type="input" name="title" size="50" /></td>
+        </tr>
+		<tr>
+            <td><label for="title">Nickname</label></td>
+            <td><input type="input" name="title" size="50" /></td>
+        </tr>
+		<tr>
+            <td><label for="title">Email Address</label></td>
+            <td><input type="input" name="title" size="50" /></td>
+        </tr>
+		<tr>
+            <td><label for="title">Home Address</label></td>
+            <td><input type="input" name="title" size="50" /></td>
+        </tr>
+		<tr>
+            <td><label for="title">Gender</label></td>
+            <td><input type="input" name="title" size="50" /></td>
+        </tr>
+		<tr>
+            <td><label for="title">Cellphone Number</label></td>
             <td><input type="input" name="title" size="50" /></td>
         </tr>
         <tr>
-            <td><label for="text">Text</label></td>
+            <td><label for="text">Comments</label></td>
             <td><textarea name="text" rows="10" cols="40"></textarea></td>
         </tr>
         <tr>
             <td></td>
-            <td><input type="submit" name="submit" value="Create news item" /></td>
+            <td><input type="submit" name="submit" value="Add" /></td>
         </tr>
     </table>    
 </form>
