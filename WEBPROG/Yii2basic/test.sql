60d61b308f3ac455bd7679204f59ef5732d4e063
Aaron Navarro <aaronnavarro.112@gmail.com>
2017-04-15 15:09:12 +0800

    Fixed database

--- WEBPROG/Yii2basic/test.sql
+++ WEBPROG/Yii2basic/test.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2017 at 05:00 AM
+-- Generation Time: Apr 07, 2017 at 05:18 AM
 -- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.8
+-- PHP Version: 7.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -38,6 +38,13 @@ CREATE TABLE `profile` (
   `slug` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`id`, `fullName`, `nickName`, `email`, `address`, `gender`, `cellphoneNumber`, `comments`, `slug`) VALUES
+(7, 'Mark Aaron C. Navarro', 'Aaron', 'aaronnavarro.112@gmail.com', 'Dasmarinas City, Cavite', 'M', 2147483647, 'Test', 'TEST');
+
 -- --------------------------------------------------------
 
 --
@@ -45,11 +52,20 @@ CREATE TABLE `profile` (
 --
 
 CREATE TABLE `trivia` (
-  `id` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL,
   `question` varchar(100) NOT NULL,
   `answer` varchar(100) NOT NULL,
   `profile_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `trivia`
+--
+
+INSERT INTO `trivia` (`id`, `question`, `answer`, `profile_id`) VALUES
+(1, '13213123', '1231231231', 7),
+(2, '113123', '123123', 7),
+(3, 'Sino mama mo?', 'Mama', 7);
 
 --
 -- Indexes for dumped tables
@@ -76,7 +92,12 @@ ALTER TABLE `trivia`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `trivia`
+--
+ALTER TABLE `trivia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
